e55002c1a5bff93600f149f7477991b3a3d53106
Hemant Chauhan <ch.hemant23@gmail.com>
2018-04-23 17:04:11 +0530

    nav without js

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,7 +4,8 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 
 
